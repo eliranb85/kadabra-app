956693dadb4071f10520d6c18bb10b48ab09c86c
eliranb85 <eliranbartist@gmail.com>
2024-04-12 09:09:40 +0300

    pages

---
+++ kadabra-app/src/app/components/Header/Header.tsx
@@ -0,0 +1,60 @@
+"use client";
+import Link from "next/link"
+import { FaUserCircle } from "react-icons/fa"
+import { MdDarkMode, MdOutlineLightMode } from "react-icons/md"
+import { useContext } from "react"
+import ThemeContext from "@/app/context/themeContext"
+const Header = () => {
+  const {darkTheme,setDarkTheme} = useContext(ThemeContext)
+  return (
+    <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between">
+      <div className="flex items-center w-full md:2/3">
+        <Link href="/" className="font-black text-tertiary-dark">
+          KADABRA
+        </Link>
+        <ul className="flex items-center space-x-4 ml-4">
+            <li className="flex items-center">
+            <Link href="/auth">
+                <FaUserCircle className="curser-pointer"/>
+            </Link>
+            </li>
+            <li className="ml-2">
+              {darkTheme?(
+                <MdOutlineLightMode className="curser-pointer" 
+onClick={()=>{
+ setDarkTheme(false); 
+ localStorage.removeItem("hotel-theme")
+}}
+                />
+                ):(
+                <MdDarkMode className="curser-pointer"
+                onClick={()=>{
+                  setDarkTheme(true); 
+                  localStorage.setItem("hotel-theme","true")
+                 }}/>
+                )}
+            </li>
+        </ul>
+      </div>
+      <ul className="flex items-center justify-between w-full md:w-1/3 mt-4">
+        <li className="hover:translate-y-2 duration-500 transition-all">
+            <Link href="/">
+                Home
+            </Link>
+        </li>
+        <li className="hover:translate-y-2 duration-500 transition-all">
+            <Link href="/dashborad">
+                Dashborad
+            </Link>
+        </li>
+        <li className="hover:translate-y-2 duration-500 transition-all">
+            <Link href="/profile">
+                Profile
+            </Link>
+        </li>
+      </ul>
+    </header>
+  )
+}
+
+export default Header
